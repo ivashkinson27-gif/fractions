e1b5eb2a06b0519dcf1266056ad4fdc149b31a7e
coolb111 <nikolaydikov856@gmail.com>
2025-11-22 23:48:19 +0300

    Update Homework 29.11.25 Dikov Nikolay.cpp

--- Homework 29.11.25 Dikov Nikolay.cpp
+++ Homework 29.11.25 Dikov Nikolay.cpp
@@ -9,10 +9,6 @@ int64_t gcd(const int64_t a, const int64_t b) {
     return gcd(b, a % b);
 }
 
-void Rational_number::showinfo() const {
-    cout << num << "/" << den << endl;
-}
-
 void Rational_number::normalize() {
     if (den == 0) {
         throw runtime_error("Denominator cannot be zero");
@@ -30,7 +26,7 @@ void Rational_number::normalize() {
 }
 Rational_number::Rational_number() : simple(0), num(0), den(1) {};
 Rational_number::Rational_number(int64_t s) : simple(s), num(s), den(1) {};
-Rational_number::Rational_number(int64_t n, int64_t d) : simple(0), num(n), den(d) {};
+Rational_number::Rational_number(int64_t n, int64_t d) : simple(0), num(n), den(d) {normalize();};
 int64_t Rational_number::get_numerator() const {
     return num;
 }
@@ -48,39 +44,53 @@ std::string Rational_number::str() const {
 Rational_number Rational_number::operator+(const Rational_number& other) const {
     int64_t new_num = num * other.den + other.num * den;
     int64_t new_den = den * other.den;
-    return {new_num, new_den};
+    Rational_number result(new_num, new_den);
+    result.normalize();
+    return result;
 }
 Rational_number Rational_number::operator-(const Rational_number& other) const {
     int64_t new_num = num * other.den - other.num * den;
     int64_t new_den = den * other.den;
-    return {new_num, new_den};
+    Rational_number result(new_num, new_den);
+    result.normalize();
+    return result;
 }
 void Rational_number::operator+=(const Rational_number& other) {
     num = num * other.den + other.num * den;
     den = den*other.den;
+    normalize();
 }
 void Rational_number::operator-=(const Rational_number& other) {
     num = num * other.den - other.num * den;
     den = den*other.den;
+    normalize();
 }
 Rational_number Rational_number::operator*(const Rational_number& other) const {
-    return {num * other.num, den * other.den};
+    int64_t new_num = num * other.num;
+    int64_t new_den = den * other.den;
+    Rational_number result(new_num, new_den);
+    result.normalize();
+    return result;
 }
 void Rational_number::operator*=(const Rational_number& other) {
     num *= other.num;
     den *= other.den;
+    normalize();
 }
 Rational_number Rational_number::operator/(const Rational_number& other) const {
     if (other.den == 0) {
         throw runtime_error("Denominator cannot be zero");
     }
-    int64_t new_num = num * other.den;
-    int64_t new_den = den * other.num;
-    return {new_num, new_den};
+    const int64_t new_num = num * other.den;
+    const int64_t new_den = den * other.num;
+    Rational_number result(new_num, new_den);
+    result.normalize();
+    return result;
 }
 void Rational_number::operator/=(const Rational_number& other) {
     num *= other.den;
     den *= other.num;
+    normalize();
 }
 Rational_number Rational_number::operator-() const {
     return {-num, den};
@@ -103,13 +113,25 @@ bool Rational_number::operator>(const Rational_number &other) const {
 bool Rational_number::operator>=(const Rational_number &other) const {
     return num*other.den >= other.num*den;
 }
-
+std::ostream & operator<<(std::ostream &os, const Rational_number &other){
+    os << other.num << "/" << other.den;
+    return os;
+}
+std::istream & operator>>(std::istream &is, Rational_number &other){
+    is >> other.num >> other.den;
+    return is;
+}
 
 
 int main() {
-    Rational_number r(1000, 7);
-    Rational_number r2(993, 1007);
-    Rational_number r3 = r.operator*(r2);
-    r3.showinfo();
+    Rational_number r(2, 3);
+    Rational_number r2(1107, 25);
+    Rational_number r3 = r-r2;
+    r -= r2;
+    r = -r;
+    cout << r << endl;
+    cout << r3 << endl;
+    Rational_number r4;
+    cin >> r4;
+    cout << r4.convert_to_double() << endl;
 }
-
